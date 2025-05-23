b08cb7df9e654a1911175d1ae0ed853816f76983
Nate Faber <nathaniel@njfaber.com>
2025-05-23 15:49:39 +0200

    remove single page

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -17,6 +16,10 @@ export default defineConfig(() => ({
   build: {
     outDir: 'dist',
     assetsInlineLimit: Infinity, // inline everything
-    cssCodeSplit: false, // do not split css
+    rollupOptions: {
+      output: {
+        manualChunks: () => 'everything.js', // single bundle
+      },
+    },
   },
 }));
