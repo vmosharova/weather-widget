3ac491cfa5e890b8c4c292aed6bf0c7b1f5343d4
Nate Faber <nathaniel@njfaber.com>
2025-05-23 15:59:28 +0200

    disallow robots

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ export default defineConfig(() => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
